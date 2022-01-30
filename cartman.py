89e7fc6d1c5929d8fb59829eb3cedba9555f3252
mihail-soltan <mihail.soltan@protonmail.ch>
2022-01-30 22:51:14 +0200

    get characters

--- cartman.py
+++ cartman.py
@@ -6,25 +6,36 @@ html = "https://southpark.fandom.com/wiki/List_of_Episodes#"
 html_page = requests.get(html, headers={'User-Agent': 'Mozilla/5.0'})
 soup = BeautifulSoup(html_page.content, 'html.parser')
 
-def grab_urls(soup):  
-    episode_urls = []  
+
+def grab_urls(soup):
+    episode_urls = []
     for season in soup.findAll('td', style="font-size:125%"):
         for episode in season.findAll('a'):
             try:
-              episode_urls.append(episode.attrs['href'])
+                episode_urls.append(episode.attrs['href'])
             except:
-                continue  
-    
+                continue
+
     return episode_urls
+
 
 urls = grab_urls(soup)
 
 for i in urls:
     if i != "/wiki/TBA":
         html = "https://southpark.fandom.com" + i + "/Script"
-        html_page = requests.get(html, headers={'User-Agent':    'Mozilla/5.0'})
+        html_page = requests.get(
+            html, headers={'User-Agent':    'Mozilla/5.0'})
         soup = BeautifulSoup(html_page.content, 'html.parser')
-        print(soup)
+        # print(html)
 
 characters = []
 lines = []
+
+for x in soup.findAll('td', {"class": "DLborderBOT DLborderRIGHT"}): 
+    if x.text.replace('\n', '') != "":
+         characters.append(x.text.replace('\n', ''))
+         lines.append(x.nextSibling.text.replace('\n', ''))
+         print(characters)
+    else:
+         continue
